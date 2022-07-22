5f23be5e9812514aab57ee6bc7620832a9c644f5
James Lindsay <jamlindsay97@gmail.com>
2022-07-22 16:08:30 +0100

    james footer done

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,80 @@ import "../css/footer.css";
 const Footer = () => {
   return (
     <div className="FooterStyled">
-      <h1 className="FooterH4Styled">Footer Banner</h1>
+      <h1 className="FooterH4Styled">2022 CodeNation M38 Group 2</h1>
+      <div className="FooterLinksStyled">
+        <a
+          href="https://github.com/alexbellairs"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          alexbellairs
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/garethhough"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          garethhough
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/harb0t"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          harb0t
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/Indykatz"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          Indykatz
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/jamlindsay97"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          jamlindsay97
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/Javabar"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          Javabar
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/jbridges373"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          jbridges373
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/KarlEvans70"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          KarlEvans70
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/Wren4Wren"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          Wren4Wren
+        </a>
+      </div>
     </div>
   );
 };
